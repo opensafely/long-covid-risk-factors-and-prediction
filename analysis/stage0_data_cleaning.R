28eac2b070acd0a1dc220b5827af4ace4b1a63c4
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-04-23 12:55:07 +0100

    Add description and re-order code

--- analysis/stage0_data_cleaning.R
+++ analysis/stage0_data_cleaning.R
@@ -16,11 +16,14 @@ library(readr); library(dplyr); library("arrow"); library(lubridate); library(ti
 input <- read_feather("output/input.feather")
 #View(input)
 
+################################################################################
+## Part 1. define index date and remove variables
+#################################################################################
 ## define cohort start date:
 index_date=as.Date("2020-12-01")
 input$index_date = as.Date(index_date)
 
-# Step 1. Define variables: COVID infection, long COVID-------------------------
+# Step 1. Define variables: COVID infection-------------------------
 # create an indicator variable for covid infection
 input$out_covid <- ifelse(is.na(input$out_covid_date), FALSE, TRUE)
 
@@ -50,7 +53,10 @@ input <- input %>% dplyr::select(patient_id, practice_id, index_date, death_date
                           colnames(input)[grepl("vax_",colnames(input))],
                           sort(tidyselect::peek_vars()))
 
-## Step 3. define variable types: factor or numerical or date--------------------
+################################################################################
+## Part 2. define variable types: factor or numerical or date
+#################################################################################
+
 # For categorical factors, specify references-----------------------------------
 cat_factors <- colnames(input)[grepl("_cat_",colnames(input))]
 input[,cat_factors] <- lapply(input[,cat_factors], function(x) factor(x, ordered = FALSE))
@@ -80,13 +86,27 @@ table(input$cov_cat_smoking_status)
 
 ## cov_cat_age_group-------------------------------------------------------------
 ## Define age groups
-input$cov_cat_age_group <- "<=18"
+input$cov_cat_age_group <- ""
 input$cov_cat_age_group <- ifelse(input$cov_num_age>=18 & input$cov_num_age<=39, "18_39", input$cov_cat_age_group)
 input$cov_cat_age_group <- ifelse(input$cov_num_age>=40 & input$cov_num_age<=59, "40_59", input$cov_cat_age_group)
 input$cov_cat_age_group <- ifelse(input$cov_num_age>=60 & input$cov_num_age<=79, "60_79", input$cov_cat_age_group)
 input$cov_cat_age_group <- ifelse(input$cov_num_age>=80, "80_105", input$cov_cat_age_group)
 input$cov_cat_age_group <- factor(input$cov_cat_age_group, ordered = TRUE)
 
+## specify date variables in the format of "%Y-%m-%d"----------------------------
+vars_dates <- grep("date", names(input))
+vars_dates <- names(input)[vars_dates]
+
+convert_to_date <- function(x){
+  as.Date(x,format = "%Y-%m-%d")
+}
+input[vars_dates] = lapply(input[vars_dates], convert_to_date)
+lapply(input[vars_dates], is.Date)
+
+## define a variable covid_history to indicate if individuals have covid infection before the start of the cohort
+input$sub_cat_covid_history <-ifelse(input$out_covid_date < input$index_date, TRUE, FALSE)
+
+#select_variables <- input %>% dplyr::select(c(sub_cat_covid_history, out_covid_date, index_date))
 
 ################################################################################
 ## Part 3. define multimorbidity: need to be carefully checked                 #
@@ -116,8 +136,6 @@ input_select <- input_select %>% mutate(cov_cat_multimorbidity =ifelse(cov_num_m
 
 input_select <- input_select %>% dplyr::select(c(patient_id, cov_cat_multimorbidity))
 
-#-----------------end of definition for multimorbidity -------------------------
-
 ## left join: keep all observations in input_select
 input <- merge(x = input_select, y = input, by = "patient_id", all.x = TRUE)
 
@@ -126,20 +144,9 @@ rm(input_select)
 ## View(input_select)
 
 
-## specify date variables in the format of "%Y-%m-%d"----------------------------
-vars_dates <- grep("date", names(input))
-vars_dates <- names(input)[vars_dates]
-
-convert_to_date <- function(x){
-  as.Date(x,format = "%Y-%m-%d")
-}
-input[vars_dates] = lapply(input[vars_dates], convert_to_date)
-lapply(input[vars_dates], is.Date)
-
-## define a variable covid_history to indicate if individuals have covid infection before the start of the cohort
-input$sub_cat_covid_history <-ifelse(input$out_covid_date < input$index_date, TRUE, FALSE)
-
-select_variables <- input %>% dplyr::select(c(sub_cat_covid_history, out_covid_date, index_date))
+#################################################################################
+## Part 4. For categorical variables, replace "na" with "Missing" as a category #
+#################################################################################
 
 ## store names of factor variables
 cov_factor_names <- names(input)[grepl("_cat", names(input))]
@@ -149,52 +156,15 @@ input_factor_vars <- input[,cov_factor_names]
 lapply(input[,cov_factor_names], is.factor)
 lapply(input_factor_vars, is.factor)
 
-## Table_0 is mainly for data check 
-output_table_0 <- function(input_factor_vars){
-    ## Summary of categorical variables
-    print("construct table_0 for data check!")
-    table_0<- data.frame()
-    for(i in cov_factor_names){
-      print(table(input_factor_vars[,i]))
-      table_0[nrow(table_0)+1,1] <- i
-      table_0[nrow(table_0),2] <- length(which(is.na(input_factor_vars[,i])))
-      #table_0[nrow(table_0),3] <- is.factor(input_factor_vars[,i])
-      table_0[nrow(table_0),3] <- NA
-      len = length(names(table(input_factor_vars[,i])))
-      index = nrow(table_0)+1
-      for(j in 1:len){
-        table_0[index-1,4+j-1]<-names(table(input_factor_vars[,i]))[j]
-        table_0[index, 4+j-1] <-table(input_factor_vars[,i])[j]
-        if(table(input_factor_vars[,i])[j] <=5){
-          table_0[index, 4+j-1] <- "[redacted]"
-        }
-      }
-    }
-    names(table_0) <- c("factor variables", "number of missing observations", "is.factor")
-    names(table_0)[4:ncol(table_0)] = rep("level name or number of observations", (ncol(table_0)-3))
-    
-    #table_0
-    
-    print("Finished constructing table_0 successfully!")
-    
-    write.csv(table_0, file="output/table_0.csv", row.names =F)
-    rmarkdown::render("analysis/compiled_table0_results.Rmd",
-                      output_file="table_0",output_dir="output")
-}
-
-
-# # For categorical variables, replace "na" with "Missing" as a category
-
 ## impose some NA and check if this works
 #input_factor_vars$cov_cat_region[1:10] =NA
 print("Replace missing values with a Missing category!")
 
-## mutate is much quicker
 input_factor_vars <- input_factor_vars %>% mutate(cov_cat_region = as.character(cov_cat_region)) %>%
   mutate(cov_cat_region = replace_na(cov_cat_region, "Missing")) %>%
   mutate(cov_cat_region = as.factor(cov_cat_region))
 
-## cov_cat_smoking_status
+# cov_cat_smoking_status
 input_factor_vars <- input_factor_vars %>% mutate(cov_cat_smoking_status = as.character(cov_cat_smoking_status)) %>%
   mutate(cov_cat_smoking_status = replace_na(cov_cat_smoking_status, "Missing")) %>%
   mutate(cov_cat_smoking_status = as.factor(cov_cat_smoking_status))
@@ -229,10 +199,53 @@ input[,cov_factor_names] <- input_factor_vars
 lapply(input[,cov_factor_names], is.factor)
 
 print("checking after replacing na with missing!") 
-output_table_0(input)
-
 for(i in cov_factor_names){
   print(table(input[,i]))
 }
 
+#################################################################################
+## Part 5. Output data set                                                       #
+#################################################################################
+
+## Table_0 is mainly for data check 
+output_table_0 <- function(input_factor_vars){
+  ## Summary of categorical variables
+  print("construct table_0 for data check!")
+  table_0<- data.frame()
+  for(i in cov_factor_names){
+    print(table(input_factor_vars[,i]))
+    table_0[nrow(table_0)+1,1] <- i
+    table_0[nrow(table_0),2] <- length(which(is.na(input_factor_vars[,i])))
+    #table_0[nrow(table_0),3] <- is.factor(input_factor_vars[,i])
+    table_0[nrow(table_0),3] <- NA
+    len = length(names(table(input_factor_vars[,i])))
+    index = nrow(table_0)+1
+    for(j in 1:len){
+      table_0[index-1,4+j-1]<-names(table(input_factor_vars[,i]))[j]
+      table_0[index, 4+j-1] <-table(input_factor_vars[,i])[j]
+      if(table(input_factor_vars[,i])[j] <=5){
+        table_0[index, 4+j-1] <- "[redacted]"
+      }
+    }
+  }
+  names(table_0) <- c("factor variables", "number of missing observations", "is.factor")
+  names(table_0)[4:ncol(table_0)] = rep("level name or number of observations", (ncol(table_0)-3))
+  
+  #table_0
+  
+  print("Finished constructing table_0 successfully!")
+  
+  write.csv(table_0, file="output/table_0.csv", row.names =F)
+  rmarkdown::render("analysis/compiled_table0_results.Rmd",
+                    output_file="table_0",output_dir="output")
+}
+
+print("Output data set!") 
+
+output_table_0(input)
+
+print("table_0 is saved successfully!") 
+
 saveRDS(input, file = "output/input_stage0.rds")
+
+print("input_stage0 is saved successfully!") 
