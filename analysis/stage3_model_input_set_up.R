40a4a92ae726fc4c0ad28d14f8f002a07f799e61
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-05-29 00:10:34 +0100

    update comments

--- analysis/stage3_model_input_set_up.R
+++ analysis/stage3_model_input_set_up.R
@@ -12,7 +12,7 @@ args <- commandArgs(trailingOnly=TRUE)
 
 if(length(args)==0){
   #analysis <- "all"          # all eligible population
-  analysis <- "vax_c"        # all eligible population but censored them by the 2nd vaccination + 14 days
+  analysis <- "vax_c"        # all eligible population but censored them by the 1st vaccination
   #analysis <- "vaccinated"   # vaccinated population
   #analysis <- "all_vax_td"    # vaccination status is included as a time-dependent covariate
   #analysis <- "infected"
