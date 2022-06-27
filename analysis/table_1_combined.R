a7f862d43d4309411a53acf3c56bcca471706882
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-06-27 23:06:46 +0100

    table 1 - testing

--- analysis/table_1_combined.R
+++ analysis/table_1_combined.R
@@ -21,24 +21,43 @@ print("Table 1 files read in successfully!")
 
 table_1_com <- rbind(table_1_all.csv, table_1_vaccinated.csv, table_1_infected.csv)
 
+index <- which(is.na(table_1_com$subgroup_level))
+table_1_com$subgroup_level[index] <- table_1_com$variable[index]
+
 table_1_wide <- reshape(table_1_com, idvar = c("variable","subgroup_level"), timevar = "analysis", direction = "wide")
+
+# according to the definition
+index <- which(table_1_wide$subgroup_level == "no_infection")
+table_1_wide$number.infected[index] = 0 # change from NA to 0 because no infection have been excluded for infected population
+index <- which(table_1_wide$variable == "sub_cat_covid_history" & 
+                 (table_1_wide$subgroup_level == "Missing" |
+                    table_1_wide$subgroup_level == "FALSE"))
+table_1_wide$number.infected[index] = 0 # change from NA to 0 because prior covid history have been excluded for infected population
 
 table_1_wide$diff_all_vax = abs(table_1_wide$number.all-table_1_wide$number.vaccinated)
 table_1_wide$diff_all_infected = abs(table_1_wide$number.all-table_1_wide$number.infected)
 
-table_1_wide <- table_1_wide %>% filter(subgroup_level!="FALSE")
+table_1_wide <- table_1_wide %>% filter(subgroup_level!="FALSE") %>%
+  filter(variable != "cov_cat_covid_phenotype")
 
 print("table_1_wide created successfully!")
 
-index <- which(!is.na(table_1_wide$diff_all_vax))
-print("Part 1")
-print(index)
-table_1_wide$diff_all_vax[index] = redactor2(table_1_wide$diff_all_vax[index])
+variables <- unique(table_1_wide$variable)
 
-index <- which(!is.na(table_1_wide$diff_all_infected))
-print("Part 2")
-print(index)
-table_1_wide$diff_all_infected[index] = redactor2(table_1_wide$diff_all_infected[index])
+for(i in variables){
+  print(i)
+  index = which(table_1_wide$variable == i)
+  
+  index2 <- which(!is.na(table_1_wide$diff_all_vax[index]))
+  table_1_wide$diff_all_vax[index2] = redactor2(table_1_wide$diff_all_vax[index2])
+  index2 <- which(is.na(table_1_wide$diff_all_vax[index]))
+  table_1_wide[index2,3:17] = "[redacted]"
+  
+  index3 <- which(!is.na(table_1_wide$diff_all_infected[index]))
+  table_1_wide$diff_all_infected[index3] = redactor2(table_1_wide$diff_all_infected[index3])
+  index3 <- which(is.na(table_1_wide$diff_all_vax[index]))
+  table_1_wide[index3,3:17] = "[redacted]"
+}
 
 # make table 1 a single file
 write.csv(table_1_wide, file="output/review/descriptives/table_1_combined.csv", row.names = F)
