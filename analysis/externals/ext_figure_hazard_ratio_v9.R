4d6d59811a83a511de405e8820e56effd02b68e4
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-08-28 12:21:56 +0100

    version 9 of hazard ratio plot with x-axis at log scale

--- analysis/externals/ext_figure_hazard_ratio_v9.R
+++ analysis/externals/ext_figure_hazard_ratio_v9.R
@@ -1,10 +1,17 @@
 # Purpose: Forest plot for hazard ratios with numerical numbers
 # Programmed by Yinghui Wei
-# 2022-08-24
+# 2022-08-28
+# Use Alimu Dyimu's package: 
 # https://cran.r-project.org/web/packages/forestploter/vignettes/forestploter-intro.html
+# https://github.com/adayim/forestploter/blob/dev/tests/testthat/test-forest.R
+
+#install.packages("devtools")
+library(devtools)
+#devtools::install_github("adayim/forestploter@dev")
+library(forestploter)
+
 library(grid)
 library(dplyr)
-library(forestploter)
 source("analysis/externals/ext_figure_hazard_ratio_v2.R")
 
 #################################################################################
@@ -21,7 +28,6 @@ df <- df %>% select(c(term,row.num,variable.x, subgroup.x, hazard_ratio.x, conf.
   rename("HR (95% CI) " = "HR (95% CI).y")%>%
   rename(Characteristic = term) 
 
-
 df[is.na(df$`HR (95% CI)`),"HR (95% CI)"] <- ""
 df[is.na(df$`HR (95% CI) `),"HR (95% CI) "] <- ""
 
@@ -31,30 +37,16 @@ df$`Post-Vaccination` <- paste(rep(" ", 25), collapse = " ")
 #df$` ` <- paste(rep(" ", nrow(df)), collapse = " ") # add empty space
 df$` ` <- paste(rep(" ", 5), collapse = " ")
 
-# Set-up theme
-tm2 <- forest_theme(base_size = 10,
-                    refline_lty = "solid",
-                    ci_pch = c(15,18),
-                    xlog = T,
-                    ci_col = c("#377eb8", "#4daf4a"),
-                    footnote_col = "blue",
-                    legend_name = "Cox Model", legend_position = "bottom",
-                    legend_value = c("Fully adjusted   ", "Age and sex adjusted"),
-                    vertline_lty = c("dashed", "dotted"),
-                    vertline_col = c("#d6604d", "#bababa"))
-
-
 #################################################################################
-## Part 2. Version 7                                                         ##
+## Part 2. Version 9                                                         ##
 #################################################################################
-# Version 7: Demographics one graph; non-demographics another graph
+# Version 9: Demographics one graph; non-demographics another graph
 #            with HR and 95%CI for age-sex adjsuted HR and fully adjusted HR on the plot
 
 # Set-up theme
 tm2 <- forest_theme(base_size = 10,
                     refline_lty = "solid",
                     ci_pch = c(15,18),
-                    xlog = T,
                     ci_col = c("#377eb8", "#4daf4a"),
                     footnote_col = "blue",
                     legend_name = "Cox Model", legend_position = "bottom",
@@ -70,13 +62,12 @@ df <- df %>% mutate(variable = ifelse(variable == "Demographics", "demographics"
 df$age_sex_aHR <- df$`HR (95% CI)`
 df$age_sex_aHR2 <- df$`HR (95% CI) `
 
-
 df <- df %>% rename("Fully aHR (95% CI)" = "HR (95% CI)") %>%
   rename("Fully aHR (95% CI) " = "HR (95% CI) ") %>%
   rename("Age and sex aHR (95%)" = age_sex_aHR) %>%
   rename("Age and sex aHR (95%) " = age_sex_aHR2) 
 
-v7_plot <- function(df,var_grp){
+v9_plot <- function(df,var_grp){
   # show in order: 1 Characteristic, 13 Space for CI, 8 = HR.x, and 14 Space for CI, 12 = HR.y
   # 16 age-sex adjusted HR; 17 age-sex adjusted HR
   df <- df %>% filter(variable==var_grp)
@@ -89,56 +80,16 @@ v7_plot <- function(df,var_grp){
                            df$conf.high.y),
               ci_column = c(2, 6),
               ref_line = 1,
+              x_trans = c("log10", "log10"),
               vert_line = c(0.5, 2),
-              xlim = c(0, 6),
-              ticks_at = c(0,1, seq(2,6,by=2)),
+              xlim = c(0, 10),
+              ticks_at = c(0.1, 0.5, 1, 2, 6, 10),
               nudge_y = 0.2,
               theme = tm2)
-  plot(p)
   cohort = "pre_vax_and_vax"
-  ggsave(file=paste0("v7_plot_HR_",cohort, "_", var_grp,".svg"), path = paste0(output_dir, "figures"),
+  ggsave(file=paste0("v9_plot_HR_",cohort, "_", var_grp,".svg"), path = paste0(output_dir, "figures"),
          plot=p, width=30, height=20)
 }
 
-v7_plot(df,var_grp="demographics")
-v7_plot(df,var_grp="non_demographics")
-
-
-#################################################################################
-## Part 2. Version 8                                                         ##
-#################################################################################
-# Version 8: Demographics one graph; non-demographics another graph
-#            with HR and 95%CI for age-sex adjsuted HR and fully adjusted HR on the plot
-
-v8_plot <- function(df,var_grp){
-  # show in order: 1 Characteristic, 13 Space for CI, 8 = HR.x, and 14 Space for CI, 12 = HR.y
-  # 16 age-sex adjusted HR; 17 age-sex adjusted HR
-  df <- df %>% filter(variable==var_grp)
-  p <- forest(df[,c(1, 13, 8, 16, 15,  14,  12, 17)],  
-              est = list(df$hazard_ratio.x,
-                         df$hazard_ratio.x,
-                         df$hazard_ratio.y,
-                         df$hazard_ratio.y),
-              lower = list(df$conf.low.x,
-                           df$conf.low.x,
-                           df$conf.low.y,
-                           df$conf.low.y), 
-              upper = list(df$conf.high.x,
-                           df$conf.high.x,
-                           df$conf.high.y,
-                           df$conf.high.y),
-              ci_column = c(2, 6),
-              ref_line = 1,
-              vert_line = c(0.5, 2),
-              xlim = c(0, 6),
-              ticks_at = c(0,1, seq(2,6,by=2)),
-              nudge_y = 0.2,
-              theme = tm2)
-  plot(p)
-  cohort = "pre_vax_and_vax"
-  ggsave(file=paste0("v8_plot_HR_",cohort, "_", var_grp,".svg"), path = paste0(output_dir, "figures"),
-         plot=p, width=30, height=20)
-}
-
-v8_plot(df,var_grp="demographics")
-v8_plot(df,var_grp="non_demographics")
+v9_plot(df,var_grp="demographics")
+v9_plot(df,var_grp="non_demographics")
