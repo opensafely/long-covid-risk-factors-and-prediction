0632f564570028dd62802e70f910cdfcaf4def60
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-09-02 17:39:53 +0100

    use long covid code

--- analysis/figure_kaplan_meier.R
+++ analysis/figure_kaplan_meier.R
@@ -14,8 +14,10 @@ levels(input$cov_cat_age_group) <- c("18-39", "40-59", "60-79", "80+")
 table(input$cov_cat_age_group)
 table(input$cov_cat_sex)
 
+index_date=as.Date("2020-01-29")
+end_date = as.Date("2022-03-31")
 # Define a function to extract KM data ---------------------------------------------------------
-function_km_data <-function(input_sex){
+function_km_data <-function(input_sex, index_date){
   dat_age <- round_km(input_sex, "lcovid_surv", "lcovid_cens", "cov_cat_age_group", threshold=6)
   dat_age$x <- dat_age$y <- dat_age$y_prob <-NA
   for(i in c("18-39", "40-59","60-79", "80+")){
@@ -30,16 +32,18 @@ function_km_data <-function(input_sex){
     dat_age_select$y_prob = dat_age_select$y/total
     dat_age$y_prob[index] <- dat_age_select$y_prob
   }
+  dat_age$time = index_date + dat_age$time
+  dat_age$x_date = index_date + dat_age$x
   return(dat_age)
 }
 # Male: Create KM data -----------------------------------------------------------------
 input_sex_m <- input %>% filter(cov_cat_sex == "M")
-dat_age_m <- function_km_data(input_sex_m)
+dat_age_m <- function_km_data(input_sex_m, index_date)
 dat_age_m$sex <- "Male"
 
 # Female: Create KM data ----------------------------------------------------------------
 input_sex_f <- input %>% filter(cov_cat_sex == "F")
-dat_age_f <- function_km_data(input_sex_f) 
+dat_age_f <- function_km_data(input_sex_f, index_date) 
 dat_age_f$sex <- "Female"
 dat_age <- rbind(dat_age_m, dat_age_f)
 # Create a supporting document to show evidence of rounding for small number suppression control
@@ -48,9 +52,9 @@ write.csv(dat_age,"output/review/descriptives/supporting_doc_km_dat_age.csv", ro
 svglite::svglite("output/review/descriptives/figure_kaplan_meier_age_sex_cum_incidence.svg", width = 9, height = 5,)
 
 ggplot(dat_age, 
-       aes(x,y, group = cov_cat_age_group, color=cov_cat_age_group, 
+       aes(x_date,y, group = cov_cat_age_group, color=cov_cat_age_group, 
            linetype = cov_cat_age_group)) +
-  geom_path() + #Ploting
+  geom_path() + #Plotting
   geom_line(aes(linetype=cov_cat_age_group),size=1)+
   scale_linetype_manual(values=c("18-39" = "longdash",
                                  "40-59" = "dotted",
@@ -60,14 +64,19 @@ ggplot(dat_age,
                                 "40-59"="#FF8000",
                                 "60-79"= "#B266FF",
                                 "80+" = "red"))+
-  labs(title="",x="\nDays since 29 January 2020", y = "\nCumulative incidence of long COVID code\n") +
+  labs(title="",x="\nDate", y = "\nCumulative incidence of long COVID code\n") +
   labs(linetype='Age group', colour="Age group") +
   facet_grid(cols = vars(sex)) +
+  scale_x_date(
+               #limits = c(index_date, end_date), 
+               date_breaks = "3 months", 
+               date_labels =  "%b %Y") +
   theme(legend.position="bottom", legend.title=element_text(size=13), 
         legend.text=element_text(size=13),
         axis.text = element_text(size = 13),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        strip.text = element_text(size = 13))
 
 dev.off()
 
@@ -75,7 +84,7 @@ dev.off()
 svglite::svglite("output/review/descriptives/figure_kaplan_meier_age_sex_cum_porobability.svg", width = 9, height = 5)
 
 ggplot(dat_age, 
-       aes(x,y_prob, group = cov_cat_age_group, color=cov_cat_age_group, 
+       aes(x_date,y_prob, group = cov_cat_age_group, color=cov_cat_age_group, 
            linetype = cov_cat_age_group)) +
   geom_path() + #Ploting
   geom_line(aes(linetype=cov_cat_age_group),size=1)+
@@ -87,13 +96,15 @@ ggplot(dat_age,
                                 "40-59"="#FF8000",
                                 "60-79"= "#B266FF",
                                 "80+" = "red"))+
-  labs(title="",x="\nDays since 29 January 2020", y = "\nCumulative probability of long COVID code\n") +
+  labs(title="",x="\nDate", y = "\nCumulative probability of long COVID code\n") +
   labs(linetype='Age group', colour="Age group") +
   facet_grid(cols = vars(sex)) +
+  scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
   theme(legend.position="bottom", legend.title=element_text(size=13), 
         legend.text=element_text(size=13),
         axis.text = element_text(size = 13),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        strip.text = element_text(size = 13))
 
 dev.off()
